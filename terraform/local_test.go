b9b0bec8d86198c91d4d674c722b589e95e2657c
Artem Volkov <a.volkov@temabit.com>
2024-03-21 14:10:33 +0100

    add test for local modules

--- terraform/local_test.go
+++ terraform/local_test.go
@@ -1,108 +1,106 @@
 package terraform
 
-// import (
-// 	"reflect"
-// 	"testing"
-// )
+import (
+	"reflect"
+	"testing"
+)
 
-// func TestGetLocalModules(t *testing.T) {
-// 	data := `{
-// 		"Modules": [
-// 			{
-// 				"Key": "",
-// 				"Source": "",
-// 				"Dir": "."
-// 			},
-// 			{
-// 				"Key": "vpc",
-// 				"Source": "gitlab.com/mycorp/vpc-module/aws",
-// 				"Version": "0.0.18",
-// 				"Dir": ".terraform/modules/vpc"
-// 			},
-// 			{
-// 				"Key": "ecs",
-// 				"Source": "gitlab.com/mycorp/ecs-module/aws",
-// 				"Version": "0.4.1",
-// 				"Dir": ".terraform/modules/ecs"
-// 			},
-// 			{
-// 				"Key": "vpc.vpc",
-// 				"Source": "registry.terraform.io/terraform-aws-modules/vpc/aws",
-// 				"Version": "5.1.2",
-// 				"Dir": ".terraform/modules/vpc.vpc"
-// 			}
-// 		]
-// 	}`
+func TestGetLocalModules(t *testing.T) {
+	data := `{
+		"Modules": [
+			{
+				"Key": "",
+				"Source": "",
+				"Dir": "."
+			},
+			{
+				"Key": "vpc",
+				"Source": "gitlab.com/mycorp/vpc-module/aws",
+				"Version": "0.0.18",
+				"Dir": ".terraform/modules/vpc"
+			},
+			{
+				"Key": "ecs",
+				"Source": "gitlab.com/mycorp/ecs-module/aws",
+				"Version": "0.4.1",
+				"Dir": ".terraform/modules/ecs"
+			},
+			{
+				"Key": "vpc.vpc",
+				"Source": "registry.terraform.io/terraform-aws-modules/vpc/aws",
+				"Version": "5.1.2",
+				"Dir": ".terraform/modules/vpc.vpc"
+			}
+		]
+	}`
 
-// 	localModules, err := GetLocalModules([]byte(data))
-// 	if err != nil {
-// 		t.Errorf(err.Error())
-// 	}
+	localModules, err := GetLocalModules([]byte(data))
+	if err != nil {
+		t.Errorf(err.Error())
+	}
 
-// 	expected := &LocalModules{
-// 		Modules: []LocalModule{
-// 			{
-// 				Source:  "gitlab.com/mycorp/vpc-module/aws",
-// 				Version: "0.0.18",
-// 			},
-// 			{
-// 				Source:  "gitlab.com/mycorp/ecs-module/aws",
-// 				Version: "0.4.1",
-// 			},
-// 			{
-// 				Source:  "registry.terraform.io/terraform-aws-modules/vpc/aws",
-// 				Version: "5.1.2",
-// 			},
-// 		},
-// 	}
-// 	if !reflect.DeepEqual(localModules, expected) {
-// 		t.Errorf("Expected %v, got %v", expected, localModules)
-// 	}
-// }
+	expected := &LocalModules{
+		Modules: []LocalModule{
+			{
+				Key:     "vpc",
+				Source:  "gitlab.com/mycorp/vpc-module/aws",
+				Version: "0.0.18",
+			},
+			{
+				Key:     "ecs",
+				Source:  "gitlab.com/mycorp/ecs-module/aws",
+				Version: "0.4.1",
+			},
+		},
+	}
+	if !reflect.DeepEqual(localModules, expected) {
+		t.Errorf("Expected %v, got %v", expected, localModules)
+	}
+}
 
-// func Test_setModuleType(t *testing.T) {
+func Test_setModuleType(t *testing.T) {
 
-// 	localModules := &LocalModules{
-// 		Modules: []LocalModule{
-// 			{
-// 				Source:  "gitlab.com/mycorp/vpc-module/aws",
-// 				Version: "0.0.18",
-// 			},
-// 			{
-// 				Source:  "gitlab.com/mycorp/ecs-module/aws",
-// 				Version: "0.4.1",
-// 			},
-// 			{
-// 				Source:  "registry.terraform.io/terraform-aws-modules/vpc/aws",
-// 				Version: "5.1.2",
-// 			},
-// 		},
-// 	}
+	localModules := &LocalModules{
+		Modules: []LocalModule{
+			{
+				Source:  "gitlab.com/mycorp/vpc-module/aws",
+				Version: "0.0.18",
+			},
+			{
+				Source:  "gitlab.com/mycorp/ecs-module/aws",
+				Version: "0.4.1",
+			},
+			{
+				Source:  "registry.terraform.io/terraform-aws-modules/vpc/aws",
+				Version: "5.1.2",
+			},
+		},
+	}
 
-// 	result := setModulesSourceType(localModules)
+	result := setModulesSourceType(localModules)
 
-// 	expected := &LocalModules{
-// 		Modules: []LocalModule{
-// 			{
-// 				Source:  "gitlab.com/mycorp/vpc-module/aws",
-// 				Version: "0.0.18",
-// 				Type:    "gitlab",
-// 			},
-// 			{
-// 				Source:  "gitlab.com/mycorp/ecs-module/aws",
-// 				Version: "0.4.1",
-// 				Type:    "gitlab",
-// 			},
-// 			{
-// 				Source:  "registry.terraform.io/terraform-aws-modules/vpc/aws",
-// 				Version: "5.1.2",
-// 				Type:    "community",
-// 			},
-// 		},
-// 	}
+	expected := &LocalModules{
+		Modules: []LocalModule{
+			{
+				Source:  "gitlab.com/mycorp/vpc-module/aws",
+				Version: "0.0.18",
+				Type:    "gitlab",
+			},
+			{
+				Source:  "gitlab.com/mycorp/ecs-module/aws",
+				Version: "0.4.1",
+				Type:    "gitlab",
+			},
+			{
+				Source:  "registry.terraform.io/terraform-aws-modules/vpc/aws",
+				Version: "5.1.2",
+				Type:    "community",
+			},
+		},
+	}
 
-// 	if !reflect.DeepEqual(result, expected) {
-// 		t.Errorf("Expected %v, got %v", expected, result)
-// 	}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, got %v", expected, result)
+	}
 
-// }
+}
