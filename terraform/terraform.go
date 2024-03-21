bfa96ec418297c9cd334454edcd3e1629d0d0bbd
Artem Volkov <a.volkov@temabit.com>
2024-03-21 16:17:35 +0100

    Add readme

--- terraform/terraform.go
+++ terraform/terraform.go
@@ -11,8 +11,6 @@ const (
 	ColorDefault = "\x1b[39m"
 	ColorRed     = "\x1b[91m"
 	ColorGreen   = "\x1b[32m"
-	ColorBlue    = "\x1b[94m"
-	ColorGray    = "\x1b[90m"
 )
 
 type TerraformModule struct {
